b9da5954cf09c39f436dfb1e239daa8335666481
dougajmcdonald <dougajmcdonald@gmail.com>
2017-06-18 08:33:55 +0100

    another path

--- GetBlogPosts/index.js
+++ GetBlogPosts/index.js
@@ -1,7 +1,7 @@
 module.exports = function (context, req) {
 
   var mongoose = require('mongoose');
-  var blogPostModel = require('./blogPostModel.js');
+  var blogPostModel = require('../Shared/blogPostModel.js');
 
   var uri = 'mongodb://dougajmcdonald:SeatCupra51@ds056559.mlab.com:56559/dougmcdonald';
   var db = mongoose.connect(uri);
